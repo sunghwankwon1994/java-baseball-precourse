1a842d205f718ef2f3f0db9aa0a3ea0f85d32723
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-19 22:35:51 +0900

    fix(generator): prevent duplicate numbers from being generated
    - Ensure number generator creates unique digits between 1 and 9
    - Prevent '3스트라이크' message from appearing when all numbers are correct
    - Modify input part line change format

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         int newGameOption = 1; // 새 게임 옵션
@@ -10,13 +13,18 @@ public class Application {
 
         while (newGameOption == 1) {
             // 1. 랜덤으로 0~9까지의 3자리 숫자 생성
-            String randomNumber = Randoms.pickNumberInRange(100, 999) + "";
-            boolean isCorrect = false;
-
-            while (!isCorrect) {
-
+            Set<Integer> uniqueNumbers = new HashSet<>();
+            StringBuilder randomNumber = new StringBuilder();
+            while (uniqueNumbers.size() < LIMIT_DIGIT) {
+                int randomDigit = Randoms.pickNumberInRange(1, 9);
+                if (uniqueNumbers.add(randomDigit)) {
+                    randomNumber.append(randomDigit);
+                }
+            }
+            //System.out.println(randomNumber);
+            while (true) {
                 // 2. 3자리 숫자 유저로부터 입력 받기
-                System.out.println("숫자를 입력해 주세요 : ");
+                System.out.print("숫자를 입력해 주세요 : ");
                 String userInput = Console.readLine();
 
                 // 7. IllegalArgumentException 예외처리 (3자리 숫자가 아닌 경우)
@@ -24,11 +32,9 @@ public class Application {
                     throw new IllegalArgumentException("잘못된 입력입니다. 3자리 숫자를 입력해야 합니다.");
                 }
                 // 3. 숫자가 랜덤 숫자와 정확히 맞았는지 체크
-                if (randomNumber.equals(userInput)) {
+                if (randomNumber.toString().equals(userInput)) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    isCorrect = true;
-                } else {
-                    System.out.println("틀렸습니다. 다시 시도하세요.");
+                    break;
                 }
                 // 4. 스트라이크와 볼 계산
                 int strikeCount = 0;
@@ -37,7 +43,7 @@ public class Application {
                 for (int i = 0; i < LIMIT_DIGIT; i++) {
                     if (userInput.charAt(i) == randomNumber.charAt(i)) {
                         strikeCount++;
-                    } else if (randomNumber.indexOf(userInput.charAt(i)) != -1) {
+                    } else if (randomNumber.toString().contains(String.valueOf(userInput.charAt(i)))) {
                         ballCount++;
                     }
                 }
