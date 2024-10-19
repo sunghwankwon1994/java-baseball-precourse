9e4e351eae2b42db1986d57d353dc17f3297269e
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-19 21:39:22 +0900

    feat(main): add random 3-digit number generator

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        // 1. 랜덤으로 0~9까지의 3자리 숫자 생성
+        String randomNumber = Randoms.pickNumberInRange(100, 999) + "";
+
+        // 2. 3자리 숫자 유저로부터 입력 받기
+        // 3. 숫자가 랜덤 숫자와 정확히 맞았는지 체크
+        // 4. 스트라이크와 볼 계산
+        // 5. 스트라이크, 볼, 낫싱 등의 결과 출력
+        // 6. 게임이 끝난 후 새 게임 여부 확인
+        // 7. IllegalArgumentException 예외처리
     }
 }
