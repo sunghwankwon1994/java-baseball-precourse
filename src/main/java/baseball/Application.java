6c6f989a452f92d287977dc8f103de9a40bb57e8
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-19 21:51:26 +0900

    feat(game): add logic to ask user for new game or quit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,46 +5,57 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
+        int newGameOption = 1; // 새 게임 옵션
         final int LIMIT_DIGIT = 3; // 자리 수 제한
 
-        // 1. 랜덤으로 0~9까지의 3자리 숫자 생성
-        String randomNumber = Randoms.pickNumberInRange(100, 999) + "";
+        while (newGameOption == 1) {
+            // 1. 랜덤으로 0~9까지의 3자리 숫자 생성
+            String randomNumber = Randoms.pickNumberInRange(100, 999) + "";
+            boolean isCorrect = false;
 
-        // 2. 3자리 숫자 유저로부터 입력 받기
-        System.out.println("숫자를 입력해 주세요 : ");
-        String userInput = Console.readLine();
+            while (!isCorrect) {
 
-        // 3. 숫자가 랜덤 숫자와 정확히 맞았는지 체크
-        if (randomNumber.equals(userInput)) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        } else {
-            System.out.println("틀렸습니다. 다시 시도하세요.");
-        }
-        // 4. 스트라이크와 볼 계산
-        int strikeCount = 0;
-        int ballCount = 0;
-        
-        for (int i = 0; i < LIMIT_DIGIT; i++) {
-            if (userInput.charAt(i) == randomNumber.charAt(i)) {
-                strikeCount++;
-            } else if (randomNumber.indexOf(userInput.charAt(i)) != -1) {
-                ballCount++;
+                // 2. 3자리 숫자 유저로부터 입력 받기
+                System.out.println("숫자를 입력해 주세요 : ");
+                String userInput = Console.readLine();
+
+                // 3. 숫자가 랜덤 숫자와 정확히 맞았는지 체크
+                if (randomNumber.equals(userInput)) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    isCorrect = true;
+                } else {
+                    System.out.println("틀렸습니다. 다시 시도하세요.");
+                }
+                // 4. 스트라이크와 볼 계산
+                int strikeCount = 0;
+                int ballCount = 0;
+
+                for (int i = 0; i < LIMIT_DIGIT; i++) {
+                    if (userInput.charAt(i) == randomNumber.charAt(i)) {
+                        strikeCount++;
+                    } else if (randomNumber.indexOf(userInput.charAt(i)) != -1) {
+                        ballCount++;
+                    }
+                }
+
+                // 5. 스트라이크, 볼, 낫싱 등의 결과 출력
+                if (strikeCount == 0 && ballCount == 0) {
+                    System.out.println("낫싱");
+                } else {
+                    if (ballCount > 0) {
+                        System.out.print(ballCount + " 볼 ");
+                    }
+                    if (strikeCount > 0) {
+                        System.out.print(strikeCount + " 스트라이크");
+                    }
+                    System.out.println();
+                }
             }
+            // 6. 게임이 끝난 후 새 게임 여부 확인
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            newGameOption = Integer.parseInt(Console.readLine());
+            // 7. IllegalArgumentException 예외처리
         }
 
-        // 5. 스트라이크, 볼, 낫싱 등의 결과 출력
-        if (strikeCount == 0 && ballCount == 0) {
-            System.out.println("낫싱");
-        } else {
-            if (ballCount > 0) {
-                System.out.print(ballCount + " 볼 ");
-            }
-            if (strikeCount > 0) {
-                System.out.print(strikeCount + " 스트라이크");
-            }
-            System.out.println();
-        }
-        // 6. 게임이 끝난 후 새 게임 여부 확인
-        // 7. IllegalArgumentException 예외처리
     }
 }
