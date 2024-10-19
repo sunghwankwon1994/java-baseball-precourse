a59b1ad37a91108a119cd527cf23dd57f692769d
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-10-19 21:41:47 +0900

    feat(compare): add logic to check if user input matches random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,7 +11,13 @@ public class Application {
         // 2. 3자리 숫자 유저로부터 입력 받기
         System.out.println("숫자를 입력해 주세요 : ");
         String userInput = Console.readLine();
+
         // 3. 숫자가 랜덤 숫자와 정확히 맞았는지 체크
+        if (randomNumber.equals(userInput)) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else {
+            System.out.println("틀렸습니다. 다시 시도하세요.");
+        }
         // 4. 스트라이크와 볼 계산
         // 5. 스트라이크, 볼, 낫싱 등의 결과 출력
         // 6. 게임이 끝난 후 새 게임 여부 확인
